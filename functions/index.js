2e67866337e966b9004876b269d89c7ee0f5d7a5
Quan Dang <18520339@gm.uit.edu.vn>
2021-03-05 22:55:07 +0700

    Validation & login

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,12 @@
 const functions = require('firebase-functions').region('asia-southeast2');
 const firebase = require('firebase').default;
+const admin = require('firebase-admin');
 const config = require('./config');
 
-const admin = require('firebase-admin');
+const validator = require('validator').default;
 const app = require('express')();
 
-firebase.initializeApp(config);
+firebase.initializeApp(config.firebaseConfig);
 admin.initializeApp();
 const firestore = admin.firestore();
 
@@ -15,10 +16,10 @@ app.get('/screams', (req, res) => {
         .orderBy('createdAt', 'desc')
         .get()
         .then(data => {
-            let screams = [];
-            data.forEach(doc => {
-                screams.push({ screamId: doc.id, ...doc.data() });
-            });
+            let screams = data.docs.map(doc => ({
+                screamId: doc.id,
+                ...doc.data(),
+            }));
             return res.json(screams);
         })
         .catch(console.error);
@@ -40,28 +41,45 @@ app.post('/scream', (req, res) => {
         })
         .catch(err => {
             console.error(err);
-            return res.status(500).json({ error: 'Something went wrong' });
+            return res.status(500).json({ error: err.code });
         });
 });
 
 app.post('/signup', (req, res) => {
-    const { email, password, confirmPassword, handle } = req.body;
-    const newUser = { email, password, confirmPassword, handle };
+    const { email, password, confirm, handle } = req.body;
+    const newUser = { email, password, confirm, handle };
+    const errors = {};
 
+    // Email
+    if (!email) errors.email = 'Email is required';
+    else if (!validator.isEmail(email)) errors.email = 'Email is invalid';
+
+    // Password
+    if (!password) errors.password = 'Password is required';
+    else if (!validator.equals(password, confirm))
+        errors.confirm = 'Password must match';
+
+    // Confirm Password
+    if (!confirm) errors.confirm = 'Confirm is required';
+    else if (!validator.equals(password, confirm))
+        errors.confirm = 'Password must match';
+
+    // Handle
+    if (!handle) errors.handle = 'Handle is required';
+
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
     firestore
-        .doc(`/users/${newUser.handle}`)
+        .doc(`/users/${handle}`)
         .get()
         .then(doc => {
             if (doc.exists)
-                return res
-                    .status(400)
-                    .json({ handle: 'This handle is already taken' });
+                return res.status(400).json({ handle: 'Handle already taken' });
             return firebase
                 .auth()
                 .createUserWithEmailAndPassword(email, password);
         })
         .then(data => {
-            firestore.doc(`/users/${newUser.handle}`).set({
+            firestore.doc(`/users/${handle}`).set({
                 email,
                 handle,
                 userId: data.user.uid,
@@ -73,9 +91,28 @@ app.post('/signup', (req, res) => {
         .catch(err => {
             console.error(err);
             if (err.code === 'auth/email-already-in-use')
-                return res
-                    .status(400)
-                    .json({ email: 'Email is already in use' });
+                return res.status(400).json({ email: 'Email already exists' });
+            return res.status(500).json({ error: err.code });
+        });
+});
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    const errors = {};
+
+    if (!email) errors.email = 'Email is required';
+    if (!password) errors.password = 'Password is required';
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(data => data.user.getIdToken())
+        .then(token => res.status(200).json({ token }))
+        .catch(err => {
+            console.error(err);
+            if (err.code === 'auth/wrong-password')
+                return res.status(400).json({ password: 'Wrong password' });
             return res.status(500).json({ error: err.code });
         });
 });
